dc229312fc7f357b7d1c54ff897d6cc5ae8562d2
JRSgit <jronaldsantos@hotmail.com>
2020-06-17 13:30:34 -0300

    add router indexBoxs

---
+++ src/pages/ListBox/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+import { Container } from "./style";
+
+export default class ListBox extends Component {
+  render() {
+    //return <Container>Hello Boxs</Container>;
+    return <Container>Olá</Container>;
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Main from './pages/Main';
-import Box from './pages/Box';
+import Main from "./pages/Main";
+import Box from "./pages/Box";
+import ListBox from "./pages/ListBox";
 
 const Routes = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" exact component={Main}/>
-            <Route path="/box/:id" component={Box}/>
-
-        </Switch>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/box/list" component={ListBox} />
+      <Route path="/box/:id" component={Box} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
